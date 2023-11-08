7b4efde6d7d5d61490787522d4dcc2d3663d6b80
bank2783 <117621142+bank2783@users.noreply.github.com>
2023-11-09 00:12:17 +0700

    commit font และ หลังบ้าน

--- NameCheckerByQRCode/routes/web.php
+++ NameCheckerByQRCode/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admins/dashboards');
 });
+
+Route::get('/login',[LoginController::class,'login_form'])->name('login');
