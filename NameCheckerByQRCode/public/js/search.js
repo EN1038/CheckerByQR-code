3f3d994ec1a3ea685fbe3b5db29ac9b081f6790c
ENX1038 <145040822+ENX1038@users.noreply.github.com>
2023-11-08 13:15:52 +0700

    commit nakub

---
+++ NameCheckerByQRCode/public/js/search.js
@@ -0,0 +1,10 @@
+var searchInput = document.getElementById('search-input');
+searchInput.style.display = 'none';
+document.getElementById('basic-addon1').addEventListener('click', function () {
+    var searchInput = document.getElementById('search-input');
+    if (searchInput.style.display === 'block') {
+        searchInput.style.display = 'none';
+    } else {
+        searchInput.style.display = 'block';
+    }
+});
--- NameCheckerByQRCode/resources/views/layouts/layout_dashboardmodren.blade.php
+++ NameCheckerByQRCode/resources/views/layouts/layout_dashboardmodren.blade.php
@@ -10,6 +10,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     {{-- End_Bootstrap --}}
 
+    {{-- Icon_Bootstraps --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    {{-- End_Icon_Bootstraps --}}
+
     {{-- CSS --}}
     <link rel="stylesheet" href="{{asset('css/styles_nav.css')}}">
     {{-- End_CSS --}}
@@ -33,7 +37,7 @@
           <div class="row">
             <div class="col border">
               <div class="d-flex flex-column">
-                <div class="">
+                <div class="fs-6 fw-medium text-muted mb-1">
                   Windows Microsoft 2023 eiei
                 </div>
                 <div class="fs-4 fw-semibold">
@@ -41,7 +45,33 @@
                 </div>
               </div>
             </div>
-            <div class="col border">34</div>
+            <div class="col border">
+              <div class="d-flex flex-row justify-content-end mb-3">
+                <div class="search">
+                  <form class="">
+                    <div class="input-group" >
+                      <input type="text" class="form-control" placeholder="Search" aria-label="Username" aria-describedby="basic-addon1" id="search-input">
+                      <span class="input-group-text btn btn-success rounded-3" id="basic-addon1"><i class="bi bi-search"></i></span>
+                    </div>
+                  </form>
+                </div>
+                <div class="home">
+                  <button type="button" class="btn btn-primary "><i class="bi bi-house"></i></button>
+                </div>
+                <div class="user">
+                  <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      <i class="bi bi-person"></i>
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li><a class="dropdown-item" href="#">Logout</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+                
+                              
+            </div>
           </div>
         </div>
       
@@ -50,6 +80,7 @@
     {{-- Script --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="{{asset('js/side_nav.js')}}"></script>
+    <script src="{{asset('js/search.js')}}"></script>
     {{-- End_Script --}}
 </body>
 </html>
