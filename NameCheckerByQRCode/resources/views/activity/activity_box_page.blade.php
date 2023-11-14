fce5106284047983ff5be50a459689f838e40971
ENX1038 <149946487+EN1038@users.noreply.github.com>
2023-11-14 23:08:19 +0700

    Add succes box-activity

--- NameCheckerByQRCode/resources/views/activity/activity_box_page.blade.php
+++ NameCheckerByQRCode/resources/views/activity/activity_box_page.blade.php
@@ -1,22 +1,21 @@
 @extends('layouts.layout_dashboardmodren')
 @section('content_body')
-<div class="text-center border">
+<div class="text-center py-4">
+    <h1 class="text-greenlight fw-bold">List Activity</h1>
     <div class="row">
         {{-- Card-Activity --}}
       <div class="col-3">
-        <a href="#" class="text-decoration-none">
         <div class="card activity-box mt-3 mb-2">
-            <div class="card-body d-flex flex-row justify-content-center align-items-center">
-                <div class="col-9 text-start content-activity-box">
-                    <span>Activity</span>
-                    <h5>ชื่อกิจกรรม</h5>
+            <div class="card-body d-flex flex-row justify-content-center align-items-center ">
+                <div class="col-8 text-start content-activity-box">
+                    <a href="#" class="text-decoration-none"><span>Activity</span><br>ชื่อกิจกรรม</a>
                 </div>
-                <div class="col-3 content-activity-box">
-                    <i class="bi bi-file-earmark-text-fill"></i>
+                <div class="col-4 content-activity-box z-2">
+                    <button class="button-edit-action-activity"><i class="bi bi-file-earmark-plus-fill"></i></button>
+                    <button class="button-trash-action-activity" onclick="navigateToURLTrash()"><i class="bi bi-trash-fill"></i></button>
                 </div>
             </div>
           </div>
-        </a>
       </div>
       {{-- End-Card-Activity --}}
     </div>
--- NameCheckerByQRCode/resources/views/layouts/layout_dashboardmodren.blade.php
+++ NameCheckerByQRCode/resources/views/layouts/layout_dashboardmodren.blade.php
@@ -182,6 +182,7 @@
     <script src="{{ asset('js/side_nav.js') }}"></script>
     <script src="{{ asset('js/search.js') }}"></script>
     <script src="{{ asset('js/script_slide.js') }}"></script>
+    <script src="{{asset('js/scripts.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.js"
     integrity="sha512-8Z5++K1rB3U+USaLKG6oO8uWWBhdYsM3hmdirnOEWp8h2B1aOikj5zBzlXs8QOrvY9OxEnD2QDkbSKKpfqcIWw=="
     crossorigin="anonymous"></script>
