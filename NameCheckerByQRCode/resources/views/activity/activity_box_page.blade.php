b29a970530cc8f64d5cfdbbbdbe760201a0f0241
bank2783 <117621142+bank2783@users.noreply.github.com>
2023-11-15 10:23:42 +0700

    tsdglsdg

--- NameCheckerByQRCode/resources/views/activity/activity_box_page.blade.php
+++ NameCheckerByQRCode/resources/views/activity/activity_box_page.blade.php
@@ -14,11 +14,9 @@
       <div class="col-3">
         <div class="card activity-box mt-3 mb-2">
             <div class="card-body d-flex flex-row justify-content-center align-items-center">
-                
-              <div class="col-9 text-start content-activity-box">
-                    <span>Activity</span>
-                    <h5>{{$row->activity_name}}</h5>
-                </div>
+              <div class="col-8 col-md-7 text-start content-activity-box">
+                <a href="#" class="text-decoration-none"><span>Activity</span><br>{{$row->activity_name}}</a>
+            </div>
                 <div class="col-4 col-md-5 content-activity-box z-2">
                     <button class="button-edit-action-activity" onclick="navigateToURLEdit()"><i class="fa-solid fa-wrench"></i></button>
                     <button class="button-trash-action-activity" onclick="navigateToURLTrash()"><i class="bi bi-trash-fill"></i></button>
--- NameCheckerByQRCode/resources/views/layouts/layout_dashboard.blade.php
+++ NameCheckerByQRCode/resources/views/layouts/layout_dashboard.blade.php
@@ -7,7 +7,7 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Dashboard</title>
-        link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
