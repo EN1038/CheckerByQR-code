e849fc86a9f99a59919017012a866f46210c8057
ENX1038 <149946487+EN1038@users.noreply.github.com>
2023-12-06 11:24:58 +0700

    update selected

--- NameCheckerByQRCode/resources/views/activity/activity_dashboard2.blade.php
+++ NameCheckerByQRCode/resources/views/activity/activity_dashboard2.blade.php
@@ -60,6 +60,7 @@
                 </div>
                 <div class="col d-none" id="noDiv_HavedataIS">
                     <select id="mySelect" onchange="addOption(event)">
+                        <option value="Option 0" disabled selected>Choose Option</option>
                         <option value="Option 1">Option 1</option>
                         <option value="Option 2">Option 2</option>
                         <option value="Option 3">Option 3</option>
