6bc9f185f6d72d5d3eb7247a778d460485b13fd2
ENX1038 <149946487+EN1038@users.noreply.github.com>
2023-12-19 19:48:53 +0700

    add validatefont-end

--- NameCheckerByQRCode/public/js/activity/activity_setting.js
+++ NameCheckerByQRCode/public/js/activity/activity_setting.js
@@ -241,6 +241,7 @@ function addInput() {
     let option_setRoundCheck_0 = document.createElement('option');
     option_setRoundCheck_0.selected = true;
     option_setRoundCheck_0.disabled = true;
+    option_setRoundCheck_0.hidden = true;
     option_setRoundCheck_0.textContent = 'เลือกจำนวนรอบ';
     select_setRoundCheck.appendChild(option_setRoundCheck_0);
 
@@ -288,6 +289,7 @@ function addInput() {
     let option_setTimeCheck_0 = document.createElement('option');
     option_setTimeCheck_0.selected = true;
     option_setTimeCheck_0.disabled = true;
+    option_setTimeCheck_0.hidden = true;
     option_setTimeCheck_0.textContent = 'เลือกเวลา';
 
     selectTimeCheck_input.appendChild(option_setTimeCheck_0);
@@ -417,7 +419,7 @@ function deleteDiv(clickedId_deleteDiv) {
         let countForm = 1;
         // เพิ่มตัวเลขลงใน value ของทุก element ที่พบ
         formDateElements.forEach(element => {
-            element.value = 'Form : '+countForm ;
+            element.value = 'Form : ' + countForm;
             countForm++;
         });
     }
@@ -603,6 +605,7 @@ document.addEventListener('change', function (event) {
         get_Selects.classList.remove('fake-disable');
         get_Selects.classList.add('revese-fake-disable');
         showResult(get_Selects);
+        disableOptionsResult(get_Selects);
     } else if (event.target.classList.contains('formFileList')) {
         let get_formfile = event.target;
         const file = get_formfile.files[0];
@@ -806,6 +809,7 @@ function createSelectOptions(event) {
         selectAndLabels.forEach(element => {
             element.remove(); // ลบ select และ label ทั้งหมดออกจาก DOM
         });
+        clearArrayValues()
     }
     let time_check = document.getElementById('selectTimeCheck' + getIdlinks).value;
     let time_start = document.getElementById('timeStart_input' + getIdlinks);
@@ -857,15 +861,18 @@ function createSelectOptions(event) {
         div.id = 'divSpace' + x + counter;
 
         var optionTitle = document.createElement('option');
-        optionTitle.value = 0;
-        optionTitle.text = 'หลังเข้าเรียน';
+        optionTitle.disabled = true;
+        optionTitle.selected = true;
+        optionTitle.hidden = true;
+        optionTitle.value = 'xx';
+        optionTitle.text = 'หลังเวลาเริ่ม';
         select.appendChild(optionTitle);
 
 
         if (time_check === '15' || time_check === '30' || time_check === '45' || time_check === '60') {
-            console.log('Option ที่ถูกเลือก: ' + time_check);
+            // console.log('Option ที่ถูกเลือก: ' + time_check);
             const intervalOneHours = 60;
-            for (let z = intervalOneHours; z <= timeDifferenceMinutes; z += intervalOneHours) {
+            for (let z = 0; z <= timeDifferenceMinutes; z += intervalOneHours) {
                 var option = document.createElement('option');
                 // เวลาที่มีอยู่เริ่มต้น
                 let [hours, minutes] = time_start.value.split(':').map(Number);
@@ -883,14 +890,14 @@ function createSelectOptions(event) {
                 // แปลงเวลากลับเป็นรูปแบบ 'hh:mm'
                 let newTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
                 option.value = z.toString();
-                option.textContent = 'หลังเข้าเรียน: ' + newTime + ' นาที'; // สร้างเนื้อหาของ option โดยใช้ฟังก์ชัน formatTime ที่คุณจะต้องสร้างขึ้น
+                option.textContent = 'หลังเวลาเริ่ม : ' + newTime + ' นาที'; // สร้างเนื้อหาของ option โดยใช้ฟังก์ชัน formatTime ที่คุณจะต้องสร้างขึ้น
                 select.appendChild(option);
             }
 
         } else if (time_check === '90' || time_check === '120') {
-            console.log('Option ที่ถูกเลือก: ' + time_check);
+            // console.log('Option ที่ถูกเลือก: ' + time_check);
             const intervalOneHours = 120;
-            for (let z = intervalOneHours; z <= timeDifferenceMinutes; z += intervalOneHours) {
+            for (let z = 0; z <= timeDifferenceMinutes; z += intervalOneHours) {
                 var option = document.createElement('option');
                 // เวลาที่มีอยู่เริ่มต้น
                 let [hours, minutes] = time_start.value.split(':').map(Number);
@@ -908,7 +915,7 @@ function createSelectOptions(event) {
                 // แปลงเวลากลับเป็นรูปแบบ 'hh:mm'
                 let newTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
                 option.value = z.toString(); // แปลงเป็น string และกำหนดค่าให้กับ option
-                option.textContent = 'หลังเข้าเรียน: ' + newTime + ' นาที'; // สร้างเนื้อหาของ option โดยใช้ฟังก์ชัน formatTime ที่คุณจะต้องสร้างขึ้น
+                option.textContent = 'หลังเวลาเริ่ม : ' + newTime + ' นาที'; // สร้างเนื้อหาของ option โดยใช้ฟังก์ชัน formatTime ที่คุณจะต้องสร้างขึ้น
                 select.appendChild(option);
             }
         }
@@ -983,9 +990,13 @@ function getDate() {
 // สร้าง array เพื่อเก็บวันที่ที่เลือกไว้แล้ว
 var selectedDates = [];
 
+function clearArrayValues() {
+    selectedOptions = {}; // ลบค่าทั้งหมดในอาเรย์
+  }
+
 function checkUniqueDate(dateValue) {
     let getIdlinks = dateValue.id.match(/\d+/g);
-    let get_dateinput = document.getElementById('date_input'+getIdlinks);
+    let get_dateinput = document.getElementById('date_input' + getIdlinks);
     // ตรวจสอบว่าวันที่ที่ผู้ใช้เลือกนี้ซ้ำกับวันที่อื่นหรือไม่
     if (selectedDates.includes(dateValue.value)) {
         get_dateinput.placeholder = 'วันที่นี้มีอยู่แล้ว โปรดเลือกวันที่อื่น';
@@ -998,7 +1009,7 @@ function checkUniqueDate(dateValue) {
 
 function checkMinDate(dateValue) {
     let getIdlinks = dateValue.id.match(/\d+/g);
-    let get_dateinput = document.getElementById('date_input'+getIdlinks);
+    let get_dateinput = document.getElementById('date_input' + getIdlinks);
     var regEx = /^\d{4}-\d{2}-\d{2}$/; // รูปแบบ YYYY-MM-DD
     var isValidFormat = dateValue.value.match(regEx) !== null;
     if (isValidFormat) {
@@ -1030,7 +1041,6 @@ function showResult(get_Selects) {
     let get_valueTimeCheck = document.getElementById('input_timeCheck' + globalIdlinks).value;
     let intValueTimeCheck = parseInt(get_valueTimeCheck);
 
-    console.log(get_idDivSpace);
     if (get_idDivSpace.id === 'divSpace' + getIdselects) {
         var selectAndLabels = get_idDivSpace.querySelectorAll('input');
         // วนลูปผ่าน select และ label แล้วลบทุกตัว
@@ -1062,6 +1072,7 @@ function showResult(get_Selects) {
     // แปลงเวลากลับเป็นรูปแบบ 'hh:mm'
     let duration_Time = `${hours_duration.toString().padStart(2, '0')}:${minutes_duration.toString().padStart(2, '0')}`;
 
+
     let Box = document.createElement('input');
     Box.setAttribute('class', 'my-4 bg-disable w-75 border-0')
     Box.disabled = true;
@@ -1084,6 +1095,47 @@ function showResult(get_Selects) {
     get_idDivSpace.appendChild(roundEnd_time);
 
 }
+
+let selectedOptions = {};
+
+function disableOptionsResult(selectElement) {
+    const selectedOption = selectElement.value;
+    
+    // ตรวจสอบว่า option ได้ถูกเลือกไว้แล้วหรือไม่
+  if (selectedOptions[selectedOption]) {
+    // alert("Option already selected in another select!");
+    selectElement.value = ''; // ล้างการเลือก
+  } else {
+    const previousValue = Object.keys(selectedOptions).find(
+      key => selectedOptions[key] === selectElement
+    );
+    
+    if (previousValue) {
+      delete selectedOptions[previousValue];
+    }
+    
+    // บันทึก option ที่ถูกเลือก
+    selectedOptions[selectedOption] = selectElement;
+  }
+
+    // อัปเดตสถานะของ options ในทุกช่อง select
+    updateOptionsAvailability();
+}
+
+function updateOptionsAvailability() {
+    let divShowTimeToUser = document.getElementById('divShowTimeToUser' + globalIdlinks);
+    let allSelects = divShowTimeToUser.querySelectorAll('select');
+  
+    allSelects.forEach(select => {
+      const selectedOption = select.value;
+  
+      // ปรับปรุง options ในทุกช่องตาม option ที่ถูกเลือก
+      const options = select.querySelectorAll('option');
+      options.forEach(option => {
+        option.disabled = selectedOptions[option.value];
+      });
+    });
+  }
 
 
 //api fetch
