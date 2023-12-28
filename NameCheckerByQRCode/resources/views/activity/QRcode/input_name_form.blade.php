6a5c30db088c0290a246495c45e622f35d7caa95
ENX1038 <149946487+EN1038@users.noreply.github.com>
2023-12-28 09:18:44 +0700

    add

--- NameCheckerByQRCode/resources/views/activity/QRcode/input_name_form.blade.php
+++ NameCheckerByQRCode/resources/views/activity/QRcode/input_name_form.blade.php
@@ -6,6 +6,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Prompt:wght@300&display=swap" rel="stylesheet"><link rel="stylesheet" href="{{asset('css/style_glassbox.css')}}">
+<script src="https://kit.fontawesome.com/4af39d0791.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="{{asset('css/style_bg.css')}}">
 <link rel="stylesheet" href="{{asset('css/style_form.css')}}">
 <div class="contrainer">
@@ -22,15 +23,16 @@
         <li></li>
       </ul>
       <div class="d-flex flex-column justify-content-center align-items-center h-100 w-100 border border-danger">
-        <div class="d-flex justify-content-center align-items-center p-5 border border-success w-75 ">
+        <div class="d-flex justify-content-center align-items-center border border-success w-75 ">
             <div class="glassmorphism-body p-5">
-                <p class="fs-1 fw-bold text-center text-success">ฟอร์มกรอกชื่อ</p>
+                <p class="fs-1 fw-bold text-center text-success"><i class="fa-solid fa-lock-open"></i> ฟอร์มกรอกชื่อ</p>
                 <p class="fs-2 fw-semi text-center text-success">กิจกรรม : {{$activity_data}}</p>
                 <form action="{{route('input_form_checker_post',request()->route()->activity_id)}}" method="post">
                     @csrf
-                    <div class="form-floating my-5">
-                        <input type="input" name="name" class="form-control" id="floatingInput" placeholder="กรุณากรอกชื่อจริง">
-                        <label for="floatingInput">กรุณากรอกชื่อจริง</label>
+                    <div class="custom-input my-5">
+                        <label for="floatingInput">กรุณากรอกชื่อจริงและนามสกุล</label>
+                        <input type="input" name="name" class="form-control" id="floatingInput" placeholder="กรุณากรอกชื่อจริงและนามสกุล">
+                        
                       </div>
                  <button type="submit" class="btn btn-success">บันทึก</button>
                  </form>
