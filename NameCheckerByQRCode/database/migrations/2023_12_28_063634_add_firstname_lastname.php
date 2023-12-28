9c54edab3bcf361d6c9112208077310eb638c438
bank2783 <117621142+bank2783@users.noreply.github.com>
2023-12-28 13:41:35 +0700

    asdadadasdasdasdada

---
+++ NameCheckerByQRCode/database/migrations/2023_12_28_063634_add_firstname_lastname.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('activity_people_registers', function (Blueprint $table) {
+            // $table->renameColumn('name','first_name');
+            $table->string('last_name')->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('activity_people_registers', function (Blueprint $table) {
+            //
+        });
+    }
+};
