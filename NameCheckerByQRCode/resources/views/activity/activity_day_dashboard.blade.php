2c8ae257bf78eb6d30edcbc65407a4a3b739f0e2
ENX1038 <149946487+EN1038@users.noreply.github.com>
2024-01-08 16:14:25 +0700

    a

--- NameCheckerByQRCode/resources/views/activity/activity_day_dashboard.blade.php
+++ NameCheckerByQRCode/resources/views/activity/activity_day_dashboard.blade.php
@@ -34,7 +34,7 @@
         
         <button data-bs-toggle="modal" type="button"  data-bs-target="#showQRcode" class="btn btn-primary btn-delete me-3"><i class="fa-solid fa-qrcode py-2 px-1 py-sm-0 px-sm-0"></i> <span class="d-none d-sm-inline">QR-Code</span></button>
         <a onclick="deleteSelected()" class="btn btn-danger btn-delete me-3"><i class="fa-solid fa-trash-can py-2 px-1 py-sm-0 px-sm-0"></i> <span class="d-none d-sm-inline">ลบกิจกรรม</span></a>
-        {{-- <a href="{{route('show_activity_dashboard',request()->route()->id)}}" class="btn btn-success me-3 me-sm-5 rounded-3 btn-setting " id="btnSetting"><i class="fa-solid fa-list-check py-2 px-1 py-sm-0 px-sm-0"></i> <span class="d-none d-sm-inline">ตั้งค่ากิจกรรม</span></a> --}}
+        <a href="{{route('show_edit_activity_dashboard',request()->route()->id)}}" class="btn btn-success me-3 me-sm-5 rounded-3 btn-setting " id="btnSetting"><i class="fa-solid fa-list-check py-2 px-1 py-sm-0 px-sm-0"></i> <span class="d-none d-sm-inline">ตั้งค่ากิจกรรม</span></a>
     </div>
     
     </div>
