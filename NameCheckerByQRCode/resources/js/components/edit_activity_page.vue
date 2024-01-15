d10fd07d949cc25a1ffefacbc1527d0c979661ca
ENX1038 <149946487+EN1038@users.noreply.github.com>
2024-01-15 09:25:00 +0700

    ass

--- NameCheckerByQRCode/resources/js/components/edit_activity_page.vue
+++ NameCheckerByQRCode/resources/js/components/edit_activity_page.vue
@@ -63,95 +63,98 @@
                 <div class="col mt-3 mb-4 text-start">
                         <span class="fw-bold fs-4 text-green">วันจัดกิจกรรม <i class="fa-regular fa-calendar-days"></i></span>
                 </div>
-            <div id="inputContainer">
-                        <!-- javacreate -->
+            
+                <div v-for="(day, index) in activity_days_setting" :key="index">
+                    <p>{{ index }}</p>
+                <div class="d-flex flex-column mb-4 text-start">
+                <div class="d-flex flex-row justify-content-between align-items-center mb-3 ">
+                    <input class="fs-4 fw-bold input-title-form" type="text" name="" value="วันที่ : " :ref="'countDate'+index">
+                    <i class="fs-3 fw-bold text-decoration-none fa-solid fa-calendar-xmark iconClose"></i>
+                </div>
+                <div class="d-flex flex-column flex-lg-row justify-content-start justify-content-lg-center align-items-start align-items-lg-center mb-3">
+                    <div class="col-12 col-lg-6">
+                        <label class="form-label fs-5">วัน/เดือน/ปี</label>
+                        <input class="form-control datepicker getDate datepicker_costom_incorrect" type="text" name="activity[date_add][date_input0][date]" placeholder="โปรดเลือกวันที่ก่อนจะใส่ข้อมูลช่องอื่น" autocomplete="off" :value="day.date" :ref="'date_input'+index">
+                    </div>
+                    <div class="col-12 col-lg-3 px-0 px-lg-3">
+                        <label class="form-label fs-5">เวลาเริ่ม</label>
+                        <input class="form-control get_IdInputTime fake-disable" disabled="" type="time" name="activity[date_add][date_input0][time][time_start]" :ref="'timeStart_input'+index" :value="day.time_start">
+                    </div>
+                    <div class="col-12 col-lg-3 px-0 px-lg-3">
+                        <label class="form-label fs-5">เวลาจบ</label>
+                        <input class="form-control get_IdInputTime fake-disable" disabled="" type="time" name="activity[date_add][date_input0][time][time_expried]" :ref="'timeEnd_input'+index" :value="day.time_expried">
+                    </div>
+                </div>
+                <div class="d-flex flex-column flex-lg-row">
+                    <label class="form-label fs-5">ตั้งค่าการเช็คชื่อ : </label>
+                    <div class="form-check ms-4">
+                        <input class="form-check-input fake-disable" type="radio" value="1" name="activity[date_add][date_input0][round_setting]" :ref="'setCheckNameAllday'+index" @click="changModeCheckName($event)">
+                        <label class="form-check-label fs-5" for="setCheckNameI0">เช็คชื่อทั้งวัน</label>
+                    </div>
+                    <div class="form-check ms-4">
+                        <input class="form-check-input fake-disable" type="radio" value="2" name="activity[date_add][date_input0][round_setting]" :ref="'setCheckNameRound'+index" @click="changModeCheckName($event)">
+                        <label class="form-check-label fs-5" for="setCheckNameII0">เช็คชื่อเป็นรอบ</label>
+                    </div>
+                    
+                </div>
+                <p>{{ 'div_checkAllday'+index }}</p>
+                <div class="d-flex justify-content-center align-items-center p-5 d-none" :ref="'div_checkAllday'+index">
+                    <h1 class="fs-3 fw-bold text-center">ระบบจะมีการเช็คชื่อตั้งแต่เวลาเริ่มจนเวลาจบ</h1>
+                </div>
+                <div :ref="'div_checkNameRound'+index" class="d-flex flex-column div_CheckName p-0 p-lg-2 d-none">
+                    <div class="d-flex flex-column-reverse flex-lg-row justify-content-start justify-content-lg-around align-itens-start align-items-lg-center w-100">
+                        <div class="col-12 col-lg-7 d-flex flex-column flex-lg-row">
+                            <label class="form-label w-100 w-lg-25 fs-5" for="selectSetRoundCheck0">จำนวนรอบ : </label>
+                        <select class="form-select h-50 w-100 w-lg-75 me-0 me-lg-3 get_IdselectRound fake-disable" disabled="" id="selectSetRoundCheck0">
+                            <option disabled="" hidden="">เลือกจำนวนรอบ</option><option value="1">จำนวน 1 รอบ</option>
+                            <option value="2">จำนวน 2 รอบ</option><option value="3">จำนวน 3 รอบ</option>
+                            <option value="4">จำนวน 4 รอบ</option><option value="5">จำนวน 5 รอบ</option>
+                        </select>
+                    </div>
+                    <div class="col-12 col-lg-5 d-flex flex-column ps-0 ps-lg-5">
+                            <label class="form-label fs-5 me-4 me-lg-0" for="selectTimeCheck0">ระยะเวลาที่เช็ค</label>
+                            <select class="form-select w-100 w-lg-75 getId_selectTimeCheck fake-disable" id="selectTimeCheck0">
+                                <option disabled="" hidden="">เลือกเวลา</option>
+                                <option value="15">15 นาที</option>
+                                <option value="30">30 นาที</option>
+                                <option value="45">45 นาที</option>
+                                <option value="60">60 นาที</option>
+                                <option value="90">90 นาที</option>
+                                <option value="120">120 นาที</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col d-flex flex-column my-2" id="divShowTimeToUser0">
+
+                    </div>
+                </div>
+                
             </div>
+        </div>
+
             <div class="d-flex flex-column flex-lg-row justify-content-center justify-content-lg-start aling-items-start aling-items-lg-center mb-5">
                 <div class="col-12 col-lg d-flex">
-                    <a class="scrollButton" ref="btnNewInput_GetId" @click="createDays()"><i class="fa-solid fa-calendar-plus"></i> สร้างวันเช็คชื่อ</a>
+                    <a class="scrollButton" ref="btnNewInput_GetId" ><i class="fa-solid fa-calendar-plus"></i> สร้างวันเช็คชื่อ</a>
                 </div>
                 <div class="col d-flex flex-row-reverse">
                     <button type="submit" class="btn-submit-setting " id="submit"><i class="fa-solid fa-cloud-arrow-up"></i> บันทึกการตั้งค่า</button>
                 </div>
             </div>      
-            
         </div>
     </div>
 </div>
-    <div class="d-flex flex-column mb-4 text-start">
-        <div class="d-flex flex-row justify-content-between align-items-center mb-3 ">
-            <input class="fs-4 fw-bold input-title-form" type="text" name="" value="วันที่ : ">
-            <i class="fs-3 fw-bold text-decoration-none fa-solid fa-calendar-xmark iconClose"></i>
-        </div>
-        <div class="d-flex flex-column flex-lg-row justify-content-start justify-content-lg-center align-items-start align-items-lg-center mb-3">
-            <div class="col-12 col-lg-6">
-                <label class="form-label fs-5">วัน/เดือน/ปี</label>
-                <input class="form-control datepicker getDate datepicker_costom_incorrect" type="text" name="activity[date_add][date_input0][date]" id="date_input0" placeholder="โปรดเลือกวันที่ก่อนจะใส่ข้อมูลช่องอื่น" autocomplete="off">
-            </div>
-            <div class="col-12 col-lg-3 px-0 px-lg-3">
-                <label class="form-label fs-5">เวลาเริ่ม</label>
-                <input class="form-control get_IdInputTime fake-disable" disabled="" type="time" name="activity[date_add][date_input0][time][time_start]" id="timeStart_input0">
-            </div>
-            <div class="col-12 col-lg-3 px-0 px-lg-3">
-                <label class="form-label fs-5">เวลาจบ</label>
-                <input class="form-control get_IdInputTime fake-disable" disabled="" type="time" name="activity[date_add][date_input0][time][time_expried]" id="timeEnd_input0">
-            </div>
-        </div>
-        <div class="d-flex flex-column flex-lg-row">
-            <label class="form-label fs-5">ตั้งค่าการเช็คชื่อ : </label>
-            <div class="form-check ms-4">
-                <input class="form-check-input fake-disable" type="radio" value="1" name="activity[date_add][date_input0][round_setting]" ref="setCheckNameAllday" @click="changModeCheckName($refs.setCheckNameAllday)">
-                <label class="form-check-label fs-5" for="setCheckNameI0">เช็คชื่อทั้งวัน</label>
-            </div>
-            <div class="form-check ms-4">
-                <input class="form-check-input fake-disable" type="radio" value="2" ref="setCheckNameRound" name="activity[date_add][date_input0][round_setting]" @click="changModeCheckName($refs.setCheckNameRound)">
-                <label class="form-check-label fs-5" for="setCheckNameII0">เช็คชื่อเป็นรอบ</label>
-            </div>
-            {{ auto_click_changModeCheckName(activity_setting.round_mode) }}
-        </div>
-        <div class="d-flex justify-content-center align-items-center p-5 d-none" ref="div_checkAllday">
-            <h1 class="fs-3 fw-bold text-center">ระบบจะมีการเช็คชื่อตั้งแต่เวลาเริ่มจนเวลาจบ</h1>
-        </div>
-        <div ref="div_checkNameRound" class="d-flex flex-column div_CheckName p-0 p-lg-2 d-none">
-            <div class="d-flex flex-column-reverse flex-lg-row justify-content-start justify-content-lg-around align-itens-start align-items-lg-center w-100">
-                <div class="col-12 col-lg-7 d-flex flex-column flex-lg-row">
-                    <label class="form-label w-100 w-lg-25 fs-5" for="selectSetRoundCheck0">จำนวนรอบ : </label>
-                <select class="form-select h-50 w-100 w-lg-75 me-0 me-lg-3 get_IdselectRound fake-disable" disabled="" id="selectSetRoundCheck0">
-                    <option disabled="" hidden="">เลือกจำนวนรอบ</option><option value="1">จำนวน 1 รอบ</option>
-                    <option value="2">จำนวน 2 รอบ</option><option value="3">จำนวน 3 รอบ</option>
-                    <option value="4">จำนวน 4 รอบ</option><option value="5">จำนวน 5 รอบ</option>
-                </select>
-            </div>
-            <div class="col-12 col-lg-5 d-flex flex-column ps-0 ps-lg-5">
-                    <label class="form-label fs-5 me-4 me-lg-0" for="selectTimeCheck0">ระยะเวลาที่เช็ค</label>
-                    <select class="form-select w-100 w-lg-75 getId_selectTimeCheck fake-disable" id="selectTimeCheck0">
-                        <option disabled="" hidden="">เลือกเวลา</option>
-                        <option value="15">15 นาที</option>
-                        <option value="30">30 นาที</option>
-                        <option value="45">45 นาที</option>
-                        <option value="60">60 นาที</option>
-                        <option value="90">90 นาที</option>
-                        <option value="120">120 นาที</option>
-                    </select>
-                </div>
-            </div>
-            <div class="col d-flex flex-column my-2" id="divShowTimeToUser0">
-
-            </div>
-        </div>
-    </div>
 </div>
 </div>
 
     
     
     
-    <div v-html="create"></div>
+    
     <h5>รหัสกิจกรรม : {{ activity_setting }}</h5>
     
     <div v-if="activity_days_setting">
         <div v-for="day in activity_days_setting" :key="day">
-            <p v-if="day">{{ get_dayId(day) }}รหัสวันที่ : {{ day.id }}{{ auto_click_createDay() }}</p>
+            <p v-if="day">{{ get_dayId(day) }}รหัสวันที่ : {{ day.id }}</p>
             <p :id="'round' + day.id"></p>
         </div>
 
@@ -171,19 +174,16 @@ export default {
             activity_setting: [],
             activity_days_setting: [],
             activity_rounds_setting: [],
-            create: '<div class="bg-primary"> อร่อย</div>',
             url:'http://127.0.0.1:8000',
-
+        
         };
     },
     mounted() {
 
         this.getActivitySetting()
         
-        
-
-
     },
+
 
     methods: {
 
@@ -207,7 +207,7 @@ export default {
                 // console.log(this.activity_days_setting);
 
 
-            },)
+            },);
 
         },
         get_dayId(newId) {
@@ -250,38 +250,46 @@ export default {
         
     },
 
-    createDays(){
-        console.log('5555555');
-        
-      
+    auto_createCount_days(){
+        var countDateElement = this.$refs.countDate;
+        var countForm = 1;
+
+        countDateElement.forEach(element => {
+            element.value = 'วันที่ : ' + countForm;
+            countForm++;
+        });
     },
 
-    auto_click_createDay(){
-        this.$refs.btnNewInput_GetId.click();
-    },
 
-    changModeCheckName(check_name){
-        if(check_name.value === '2'||check_name === '2'){
-            this.$refs.div_checkAllday.classList.add('d-none');
-            this.$refs.div_checkNameRound.classList.remove('d-none');
-        }else if(check_name.value === '1'||check_name === '1'){
-            this.$refs.div_checkNameRound.classList.add('d-none');
-            this.$refs.div_checkAllday.classList.remove('d-none');
-        }
-    },
-
-    auto_click_changModeCheckName(check_mode){
-        
-        if(check_mode === '1'){
-            this.$refs.setCheckNameAllday.checked = true;
-            this.changModeCheckName(check_mode);
-        }else if(check_mode === '2'){
-            this.$refs.setCheckNameRound.checked = true;
-            this.changModeCheckName(check_mode);
+    changModeCheckName(event){
+        console.log(event.target.ref)
+        if(event){
+                if(event.target.value === '2'||event === '2'){
+                    this.$refs.div_checkAllday0.classList.add('d-none');
+                    this.$refs.div_checkNameRound0.classList.remove('d-none');
+                }else if(event.target.value === '1'||event === '1'){
+                    this.$refs.div_checkNameRound0.classList.add('d-none');
+                    this.$refs.div_checkAllday0.classList.remove('d-none');
+                }else{
+                    console.log('error');
+                }
         }else{
-            console.log('erroe');
-        }
+            console.log('ไม่มีข้อมูลส่งมา');
+        };
+        
     },
+
+    auto_click_changModeCheckName(check_mode) {
+                if(check_mode === '1') {
+                        this.$refs.setCheckNameAllday0.checked = true;
+                        this.changModeCheckName(check_mode);
+                }else if(check_mode === '2') {
+                        this.$refs.setCheckNameRound0.checked = true;
+                        this.changModeCheckName(check_mode);
+                }else {
+                    console.log('error');
+                }
+        },
 
     },
 }
