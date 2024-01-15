c180e74b138e331c6aa38a10b1f299e5a92a02af
bank2783 <117621142+bank2783@users.noreply.github.com>
2024-01-15 09:35:55 +0700

    commit

--- NameCheckerByQRCode/resources/views/activity/activity_day_dashboard.blade.php
+++ NameCheckerByQRCode/resources/views/activity/activity_day_dashboard.blade.php
@@ -89,11 +89,11 @@
 
 
 <script src="{{ asset('js/activity/activity_day_dashboard.js') }}"></script>
-@endsection
+{{-- @endsection
 
 @foreach ( as )
   
-@endforeach
+@endforeach --}}
 
 @foreach ($activity_day_array as $row )
 <div class="modal fade" id="{{'edit_btn_'.$row->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
