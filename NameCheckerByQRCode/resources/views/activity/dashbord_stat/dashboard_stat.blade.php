a185197fddb991c515bdda8d1027bc1dc86bfd38
ENX1038 <149946487+EN1038@users.noreply.github.com>
2024-02-13 09:55:56 +0700

    s

--- NameCheckerByQRCode/resources/views/activity/dashbord_stat/dashboard_stat.blade.php
+++ NameCheckerByQRCode/resources/views/activity/dashbord_stat/dashboard_stat.blade.php
@@ -2,14 +2,24 @@
 @section('content_body')
 
 
-<div class="row">
-    <div class="col">
-        ชื่อกิจกรรม
-    </div>
-    <div class="col">
-        aa
-        
-    </div>
+<div>
+    
+    <div class="d-flex flex-column mb-3">
+        <div class="p-2">
+            <h1 class="text-greenlight fw-bold text-center">สถิติกิจกรรม</h1>
+        </div>
+        <div class="d-flex flex-row mb-3">
+            <div class="p-2">
+                Flex item 1
+            </div>
+            <div class="p-2">
+                Flex item 2
+            </div>
+            <div class="p-2">
+                Flex item 3
+            </div>
+        </div>
+      </div>
 </div>
 
 @endsection
